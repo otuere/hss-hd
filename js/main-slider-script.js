fe6ce99f468814d6b807228250d9e01b475de214
Okan Türe <okanture@hotmail.com>
2024-12-05 12:01:17 +0100

    slider touch icons buttons removed

--- js/main-slider-script.js
+++ js/main-slider-script.js
@@ -24,26 +24,6 @@
                         swipe_min_touches: 1,
                         swipe_direction: "horizontal",
                         drag_block_vertical: false
-                    },
-                    arrows: {
-                        style:"uranus",
-                        enable:true,
-                        hide_onmobile:true,
-                        hide_under:600,
-                        hide_onleave:false,
-                        tmp:'',
-                        left: {
-                            h_align:"left",
-                            v_align:"center",
-                            h_offset:0,
-                            v_offset:0
-                        },
-                        right: {
-                            h_align:"right",
-                            v_align:"center",
-                            h_offset:0,
-                            v_offset:0
-                        }
                     }
                     
                 },
@@ -101,47 +81,6 @@
                         swipe_min_touches: 1,
                         swipe_direction: "horizontal",
                         drag_block_vertical: false
-                    },
-                    arrows: {
-                        enable:true,
-                        style: 'erinyen',
-                        tmp: '<div class="tp-title-wrap"><div class="tp-arr-imgholder"></div><div class="tp-arr-img-over"></div><span class="tp-arr-titleholder">Hausmeister Service</span></div>',
-                        
-                        left: {
-                            h_align:"left",
-                            v_align:"center",
-                            h_offset:20,
-                            v_offset:0
-                        },
-                        right: {
-                            h_align:"right",
-                            v_align:"center",
-                            h_offset:20,
-                            v_offset:0
-                        }
-                    },
-                    bullets: {
- 
-                        enable: true,
-                        style: 'hebe',
-                        tmp: '<span class="tp-bullet-image"></span>',
-                        direction: 'horizontal',
-                        rtl: false,
-                 
-                        container: 'slider',
-                        h_align: 'center',
-                        v_align: 'bottom',
-                        h_offset: 0,
-                        v_offset: 30,
-                        space: 10,
-                 
-                        hide_onleave: false,
-                        hide_onmobile: false,
-                        hide_under: 0,
-                        hide_over: 9999,
-                        hide_delay: 200,
-                        hide_delay_mobile: 1200
-                 
                     }
                     
                 },
